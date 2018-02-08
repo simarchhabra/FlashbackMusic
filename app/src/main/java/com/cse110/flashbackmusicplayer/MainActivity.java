a5b73bc857eb8bde33a130fd6049f2849e4f5254
valegonzalez <vdg001@ucsd.edu>
2018-02-07 21:47:28 -0800

    Issue 25: Display songs (done with 4 tracks)

--- app/src/main/java/com/cse110/flashbackmusicplayer/MainActivity.java
+++ app/src/main/java/com/cse110/flashbackmusicplayer/MainActivity.java
@@ -1,13 +1,73 @@
 package com.cse110.flashbackmusicplayer;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> songsList;
+
+    ListView songsView;
+
+    ListAdapter adapter;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songsView = (ListView) findViewById(R.id.songsView);
+
+        songsList = new ArrayList<>();
+
+        Field[] fields = R.raw.class.getFields();
+        for (int i = 0; i < fields.length; i++) {
+            songsList.add(fields[i].getName());
+        }
+
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songsList);
+        songsView.setAdapter(adapter);
+
+        songsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+
+                int resID = getResources().getIdentifier(songsList.get(i), "raw", getPackageName());
+
+                mediaPlayer = MediaPlayer.create(MainActivity.this, resID);
+
+                mediaPlayer.start();
+            }
+        });
     }
+
 }
