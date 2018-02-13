50b69c1fd6d264d6e8ecb0d1b94478dfb9b5d382
simarchhabra <simar.chhabra@gmail.com>
2018-02-13 07:21:57 -0800

    Issues #27, 29, 31, 65: Created pause button functionality. Stored metadata and also displayed album cover art. Navigate in between windows.

--- app/src/main/java/com/cse110/flashbackmusicplayer/CurrentTrackDisplay.java
+++ app/src/main/java/com/cse110/flashbackmusicplayer/CurrentTrackDisplay.java
@@ -1,16 +1,23 @@
 package com.cse110.flashbackmusicplayer;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.media.MediaPlayer;
+
+import org.w3c.dom.Text;
 
 public class CurrentTrackDisplay extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     Button switchScreen;
     Button pauseButton;
     boolean pauseDisplayed = true;
@@ -22,23 +29,51 @@ public class CurrentTrackDisplay extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // access metadata
+        String[] metadata = getIntent().getStringArrayExtra("METADATA");
+
+        // access and display title, artist metadata
+        TextView songTitle = (TextView) findViewById(R.id.songTitle);
+        String songTitleStr= metadata[0] + "\n" + metadata[2];
+        songTitle.setText(songTitleStr);
+
+        // access and display album, track number metadata
+        TextView songAlbum = (TextView) findViewById(R.id.songAlbum);
+        String songAlbumStr = metadata[1] + "\nTrack #: " + metadata[3];
+        songAlbum.setText(songAlbumStr);
+
+        // create image for album cover
+        ImageView albumcover = (ImageView) findViewById(R.id.album_cover);
+        byte[] albumArtData = getIntent().getByteArrayExtra("ALBUM_ART");
+        if (albumArtData != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(albumArtData, 0, albumArtData.length);
+            albumcover.setImageBitmap(bitmap);
+            albumcover.setAdjustViewBounds(true);
+        }
+        else {
+            albumcover.setImageResource(R.drawable.nocover);
+            albumcover.setAdjustViewBounds(true);
+        }
+
         switchScreen = (Button) findViewById(R.id.backButton);
         pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        // call media player
+        launchInit();
 
         pauseButton.setOnClickListener(
                 new View.OnClickListener(){
                     @Override
                     public void onClick(View view){
-                        /* TODO: switching display to current track display
-                        //launchPlayPause();
+                        launchPlayPause();
                         if(pauseDisplayed) {
-                            pauseButton.setBackground(drawable.play);
+                            pauseButton.setBackgroundResource(R.drawable.play);
                             pauseDisplayed = false;
                         }
                         else{
-                            pauseButton.setBackground(drawable.pause);
+                            pauseButton.setBackgroundResource(R.drawable.pause);
                             pauseDisplayed = true;
-                        } */
+                        }
                     }
                 }
         );
@@ -47,27 +82,34 @@ public class CurrentTrackDisplay extends AppCompatActivity {
         switchScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
                 finish();
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     /**
-     * TODO: Method to enable play and pause by passing intent to pause or play mediaPlayer
-     * to MainActivity.java
+     * Method for launching playback
+     */
+    public void launchInit() {
+        int resID = getIntent().getIntExtra("RES_ID", 0);
+        mediaPlayer = MediaPlayer.create(CurrentTrackDisplay.this, resID);
+        mediaPlayer.start();
+    }
+
+    /**
+     * Method to enable play and pause
      */
     public void launchPlayPause(){
-        Intent intent2 = new Intent(this, MainActivity.class);
-        startActivity(intent2);
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
+        else {
+            mediaPlayer.start();
+        }
     }
 
 }
