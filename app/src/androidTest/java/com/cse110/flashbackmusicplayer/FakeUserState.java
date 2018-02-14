b7c36ddc8f8ce1bb2e3eb656ca0939155a3b9eac
Mike <michael.boulgakov@gmail.com>
2018-02-14 00:42:59 -0800

    #35 Created Unit Test for Song.java

---
+++ app/src/androidTest/java/com/cse110/flashbackmusicplayer/FakeUserState.java
@@ -0,0 +1,34 @@
+package com.cse110.flashbackmusicplayer;
+
+import android.location.Location;
+
+public class FakeUserState extends UserState {
+    // The current up-to-date position of the user and the name of the closest location.
+    Location location = new Location("gps");
+    String place = "";
+    TimeSegment timeSegment = TimeSegment.MORNING;
+    int dayOfWeek = 1;
+    String date = "";
+    String time = "";
+    long systemTime = 0;
+
+    public FakeUserState() {}
+
+    public Location getLocation() { return new Location(location); }
+    public String getPlace() { return place; }
+    public TimeSegment getTimeSegment() { return timeSegment; }
+    public int getDayOfWeek() { return dayOfWeek; }
+    public String getDate() { return date; }
+    public String getTime() { return time; }
+    public long getSystemTime() {
+        return systemTime;
+    }
+
+    public void setLocation(Location location) { this.location = location; }
+    public void setPlace(String place) { this.place = place; }
+    public void setTimeSegment(TimeSegment timeSegment) { this.timeSegment = timeSegment; }
+    public void setDayOfWeek(int dayOfWeek) { this.dayOfWeek = dayOfWeek; }
+    public void setDate(String date) { this.date = date; }
+    public void setTime(String time) { this.time = time; }
+    public void setSystemTime(long systemTime) { this.systemTime = systemTime; }
+}
---
+++ app/src/androidTest/java/com/cse110/flashbackmusicplayer/SongClassTest.java
@@ -0,0 +1,219 @@
+package com.cse110.flashbackmusicplayer;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+
+public class SongClassTest {
+
+    private FakeUserState state;
+
+    @Before
+    public void init() {
+        state = new FakeUserState();
+    }
+
+    @Test
+    public void startedPlaying() throws Exception {
+        // Play a song in the following state.
+        byte[] albumCover = {0, 1, 0, 1};
+        Song song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+
+        state.setDayOfWeek(Calendar.SUNDAY);
+        state.setTimeSegment(TimeSegment.NOON);
+        Location loc = new Location(LocationManager.GPS_PROVIDER);
+        loc.setLatitude(1.0); loc.setLongitude(-1.0);
+        state.setLocation(loc);
+
+        String time = "23:32:16"; state.setTime(time);
+        String date = "2/13/2018"; state.setDate(date);
+        String place = "Revelle, UCSD"; state.setPlace(place);
+        long sysTime = 100121503; state.setSystemTime(sysTime);
+
+        song.startedPlaying(state);
+
+        // Check if the song object, remembers all of this.
+        assertEquals(time, song.getTime());
+        assertEquals(date, song.getDate());
+        assertEquals(place, song.getPlace());
+        assertEquals(sysTime, song.getSystemTime());
+
+        boolean[] daysOfWeek = new boolean[7];
+        daysOfWeek[Calendar.SUNDAY - 1] = true;
+        assertArrayEquals(daysOfWeek, song.getDaysOfWeek());
+
+        boolean[] timeSegments = new boolean[TimeSegment.numSegments];
+        timeSegments[TimeSegment.NOON.getIndex()] = true;
+        assertArrayEquals(timeSegments, song.getTimeSegments());
+
+        Location comp = song.getLocations().get(0);
+        assertTrue(loc.getLatitude() == comp.getLatitude());
+        assertTrue(loc.getLongitude() == comp.getLongitude());
+    }
+
+    @Test
+    public void getDaysOfWeek() throws Exception {
+        boolean[] daysOfWeek = new boolean[7];
+        // Test not playing the song on any day of the week.
+        byte[] albumCover = {0, 1, 0, 1};
+        Song song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        assertArrayEquals(daysOfWeek, song.getDaysOfWeek());
+
+        // Test playing once on Sunday.
+        state.setDayOfWeek(Calendar.SUNDAY);
+        daysOfWeek[0] = true;
+        song.startedPlaying(state);
+        assertArrayEquals(daysOfWeek, song.getDaysOfWeek());
+
+        // Play a song on Monday and Tuesday.
+        song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        daysOfWeek = new boolean[7];
+        // monday
+        state.setDayOfWeek(Calendar.MONDAY);
+        daysOfWeek[1] = true;
+        song.startedPlaying(state);
+        // tuesday
+        state.setDayOfWeek(Calendar.TUESDAY);
+        daysOfWeek[2] = true;
+        song.startedPlaying(state);
+        assertArrayEquals(daysOfWeek, song.getDaysOfWeek());
+
+        // Play a song on all 3 days of the week.
+        song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        daysOfWeek = new boolean[7];
+        for (int i = 0; i < 7; i++) {
+            state.setDayOfWeek(i + 1);
+            daysOfWeek[i] = true;
+            song.startedPlaying(state);
+        }
+        assertArrayEquals(daysOfWeek, song.getDaysOfWeek());
+    }
+
+    @Test
+    public void getTimeSegments() throws Exception {
+        boolean[] timeSegments = new boolean[TimeSegment.numSegments];
+        // Test not playing the song in any segment.
+        byte[] albumCover = {0, 1, 0, 1};
+        Song song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        assertArrayEquals(timeSegments, song.getTimeSegments());
+
+        // Test playing in the morning.
+        state.setTimeSegment(TimeSegment.MORNING);
+        timeSegments[TimeSegment.MORNING.getIndex()] = true;
+        song.startedPlaying(state);
+        assertArrayEquals(timeSegments, song.getTimeSegments());
+
+        // Play a song in the noon and evening.
+        song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+
+        // noon
+        timeSegments = new boolean[TimeSegment.numSegments];
+        state.setTimeSegment(TimeSegment.NOON);
+        timeSegments[TimeSegment.NOON.getIndex()] = true;
+        song.startedPlaying(state);
+        // evening
+        state.setTimeSegment(TimeSegment.EVENING);
+        timeSegments[TimeSegment.EVENING.getIndex()] = true;
+        song.startedPlaying(state);
+        assertArrayEquals(timeSegments, song.getTimeSegments());
+
+        // Play a song in all 3 sections of the day.
+        song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        // morning
+        timeSegments = new boolean[TimeSegment.numSegments];
+        state.setTimeSegment(TimeSegment.MORNING);
+        timeSegments[TimeSegment.MORNING.getIndex()] = true;
+        song.startedPlaying(state);
+        // noon
+        state.setTimeSegment(TimeSegment.NOON);
+        timeSegments[TimeSegment.NOON.getIndex()] = true;
+        song.startedPlaying(state);
+        // evening
+        state.setTimeSegment(TimeSegment.EVENING);
+        timeSegments[TimeSegment.EVENING.getIndex()] = true;
+        song.startedPlaying(state);
+        assertArrayEquals(timeSegments, song.getTimeSegments());
+    }
+
+    @Test
+    public void getLocations() throws Exception {
+        // Create a bunch of different locations.
+        ArrayList<Location> locations = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Location location = new Location(LocationManager.GPS_PROVIDER);
+            location.setLongitude(i*i*0.14 - 6.09*i + 14.605);
+            location.setLatitude(-i*i*5.14 + 110.11*i + 9.403);
+            locations.add(location);
+        }
+
+        // "Play" the song in these locations.
+        byte[] albumCover = {0, 1, 0, 1};
+        Song song = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        for (Location location : locations) {
+            state.setLocation(location);
+            song.startedPlaying(state);
+        }
+
+        // Check if all the locations we played were added to the array.
+        for (int i = 0; i < locations.size(); i++) {
+            Location comp = song.getLocations().get(i);
+            Location loc = locations.get(i);
+            assertTrue(loc.getLatitude() == comp.getLatitude());
+            assertTrue(loc.getLongitude() == comp.getLongitude());
+        }
+    }
+
+    @Test
+    public void getters() throws Exception {
+        // Create a couple random songs using the first constructor.
+        byte[] albumCover = {0, 1, 0, 1};
+        Song s1 = new Song("filename", "title", "album", "artist", "trackNumber", albumCover);
+        Song s2 = new Song("_123_go", "123 go", "New and Best of Keaton Simons", "Keaton Simons",
+                "1/10", albumCover);
+
+        // Create using the second constructor.
+        byte[] albumCover2 = {1, 1, 1, 0, 0, 1, 0, 1};
+        Song s3 = new Song("filename", "title", "album", "artist", "trackNumber", "genre",
+                "comments about song", 1000, albumCover2);
+        Song s4 = new Song("_123_go", "123 go", "New and Best of Keaton Simons", "Keaton Simons",
+                "1/10", "country", "very good song", 2010, albumCover2);
+
+        // Make sure that all of the getters return the correct field.
+        assertEquals("filename", s1.getFilename());
+        assertEquals("title", s1.getTitle());
+        assertEquals("album", s1.getAlbum());
+        assertEquals("artist", s1.getArtist());
+        assertEquals("trackNumber", s1.getTrackNumber());
+        assertArrayEquals(albumCover, s1.getAlbumCover());
+
+        assertEquals("_123_go", s2.getFilename());
+        assertEquals("123 go", s2.getTitle());
+        assertEquals("New and Best of Keaton Simons", s2.getAlbum());
+        assertEquals("Keaton Simons", s2.getArtist());
+        assertEquals("1/10", s2.getTrackNumber());
+        assertArrayEquals(albumCover, s2.getAlbumCover());
+
+        assertEquals("filename", s3.getFilename());
+        assertEquals("title", s3.getTitle());
+        assertEquals("album", s3.getAlbum());
+        assertEquals("artist", s3.getArtist());
+        assertEquals("trackNumber", s3.getTrackNumber());
+        assertArrayEquals(albumCover2, s3.getAlbumCover());
+
+        assertEquals("_123_go", s4.getFilename());
+        assertEquals("123 go", s4.getTitle());
+        assertEquals("New and Best of Keaton Simons", s4.getAlbum());
+        assertEquals("Keaton Simons", s4.getArtist());
+        assertEquals("1/10", s4.getTrackNumber());
+        assertArrayEquals(albumCover2, s4.getAlbumCover());
+    }
+
+}
