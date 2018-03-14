671d4168a9bd83502d583bf376bb27cab7e4fc71
simarchhabra <simar.chhabra@gmail.com>
2018-03-13 20:15:09 -0700

    Added Google Login Functionality

---
+++ app/src/main/java/com/cse110/flashbackmusicplayer/PersonService.java
@@ -0,0 +1,16 @@
+package com.cse110.flashbackmusicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PeopleService extends Service {
+    public PeopleService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/cse110/flashbackmusicplayer/UserSystem.java
@@ -0,0 +1,8 @@
+package com.cse110.flashbackmusicplayer;
+
+/**
+ * Created by simarchhabra on 3/13/18.
+ */
+
+public class UserSystem {
+}
