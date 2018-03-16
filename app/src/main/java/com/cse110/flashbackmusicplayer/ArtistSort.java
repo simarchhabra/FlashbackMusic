adb900e915510505cad43713822d7dc2e1943af3
Mike <michael.boulgakov@gmail.com>
2018-03-16 15:01:23 -0700

    Fixed sort methods to no longer overwrite songs.

--- app/src/main/java/com/cse110/flashbackmusicplayer/ArtistSort.java
+++ app/src/main/java/com/cse110/flashbackmusicplayer/ArtistSort.java
@@ -1,6 +1,7 @@
 package com.cse110.flashbackmusicplayer;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static com.cse110.flashbackmusicplayer.MainActivity.songDB;
@@ -8,17 +9,10 @@ import static com.cse110.flashbackmusicplayer.MainActivity.songDB;
 
 public class ArtistSort implements SortStrategy{
     public ArrayList<String> sort(List<String> input) {
-        ArrayList<String> artSorted = new ArrayList<>(input);
-        for(int i = 1; i < artSorted.size(); i++) {
-            Song song = songDB.get(artSorted.get(i));
-            String val = song.getAlbum();
-            int j = i - 1;
-            while( j > -1 && song.getArtist().compareTo(val) > 0) {
-                artSorted.set(j + 1, artSorted.get(j));
-                j--;
-            }
-            artSorted.set(j+1, artSorted.get(i));
-        }
-        return artSorted;
+        ArrayList<String> sorted = new ArrayList<>(input);
+
+        sorted.sort(Comparator.comparing(s -> songDB.get(s).getArtist()));
+
+        return sorted;
     }
 }
